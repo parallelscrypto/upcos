68a9a2e66a31b5871a9c952dde896dc25cc86d2f
DJUPC <dj@upcbandradio>
2023-06-20 15:18:59 -0700

    added k command

--- src/components/MyTerminal.js
+++ src/components/MyTerminal.js
@@ -1959,6 +1959,30 @@ var playButton =
             },
 
 
+            k: {
+		    description: '<p style="color:hotpink;font-size:1.1em">** Open chat client window in draggable interface</p>',
+              fn: (user) => {
+
+		      var fullUrl = "https://kick.com/" + user;
+                      var winNum = "0";
+                      var mplayer = this.getMplayer(fullUrl);
+                      if(winNum == "0") {
+		         this.setState(prevState => ({ fullIpfs: mplayer }));
+		         this.setState(prevState => ({ pipVisibility: !prevState.pipVisibility }));
+		         this.setState(prevState => ({ pipDisplay: !prevState.pipDisplay}));
+                      }
+                      else if(winNum == "1") {
+		         this.setState(prevState => ({ fullIpfs2: mplayer }));
+		         this.setState(prevState => ({ pipVisibility2: !prevState.pipVisibility2 }));
+		         this.setState(prevState => ({ pipDisplay2: !prevState.pipDisplay2}));
+                      }
+ 
+              }
+            },
+
+
+
+
 
 
             com: {
